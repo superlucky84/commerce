ff83cbad656501ec15b77203d6a97f8e94532960
jhkim105 <jhkim105@gmail.com>
2017-06-27 17:35:43 +0900

    .

---
+++ src/test/java/com/commerce/entity/CartProductTest.java
@@ -0,0 +1,13 @@
+package com.commerce.entity;
+
+/**
+ * Created by jihwan on 27/06/2017.
+ */
+public class CartProductTest {
+
+
+  public void test() {
+    CartProduct.Id id = new CartProduct.Id(1L, 1L);
+
+  }
+}
